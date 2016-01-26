23888f6ece79d0496ee9739568cca74768c93012
Matthew Sauve <samatthe@umich.edu>
2016-01-26 00:27:40 -0500

    Finished parts 4 and 5

--- list.c
+++ list.c
@@ -10,7 +10,32 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
-
+	list_t *previous;
+	list_t *current;
+	if(new_element->index <= head->index)
+	{
+		new_element->next = head;
+		head = new_element;
+	}
+	else
+	{
+		previous = head;
+		while(previous != NULL && previous->index < new_element->index)
+		{
+			current = previous;
+			previous = previous->next;
+		}
+		if(previous != NULL)
+		{
+			new_element->next = current;
+			current->next = new_element;
+		}
+		else
+		{
+			new_element->next = NULL;
+			current->next = new_element;
+		}
+	}
 	return head;
 }
 
@@ -18,7 +43,17 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
-
+	list_t *temp = NULL;
+	list_t *last = head->next;
+	list_t *tempHead = head;
+	while(last != NULL)
+	{
+		temp = last->next;
+		last->next = tempHead;
+		tempHead = last;
+		last = temp;
+	}
+	head = tempHead;
 	return head;
 }
 
